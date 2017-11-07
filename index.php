ca578b965071448dc0635b43f26a0e07d88f504e
isratjahan <jahanaranishat04@gmail.com>
2017-11-07 12:46:42 +0600

    this is crud

---
+++ index.php
@@ -0,0 +1,8 @@
+<?php require 'header.php'; ?>
+    <div class="container mt-5">
+        <ul class="list-group">
+            <li class="list-group-item"><a href="create.php">create</a></li>
+            <li class="list-group-item"><a href="show.php">show</a></li>
+        </ul>
+    </div>
+<?php require 'footer.php'; ?>
